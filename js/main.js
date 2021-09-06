bb57f9779c0b4cf136b581f7b71c9201b4fba2c8
Thijs Vlaeyen <thijs@mms-solutions.be>
2021-09-06 17:02:12 +0200

    push noti

--- js/main.js
+++ js/main.js
@@ -6,3 +6,45 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geo Location not supported by browser");
+  }
+}
+//function that retrieves the position
+function showPosition(position) {
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  const notifTitle = "Belangrijke mededeling"
+  const notifBody = "Locatie is gedeeld:" + location.longitude + "\n" + location.latitude;
+  const options = {
+    body: notifBody,
+    icon: `favicon.ico`,
+    vibrate: [100]
+  }
+  navigator.serviceWorker.getRegistration().then(function(reg) {
+    reg.showNotification(notifTitle, options);
+  });
+  console.log(location)
+}
+
+let interval;
+function shareLocation() {
+  Notification.requestPermission()
+  interval = window.setInterval(getLocation, 5000, 15);
+}
+
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
