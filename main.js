c043c0ed3cfce33cf9bbaceb09b5f4629e74f1e5
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2022-03-28 22:53:11 +0200

    ttt

---
+++ main.js
@@ -0,0 +1,27 @@
+var times = ["23:30", "15:56", "00:00"];
+function fn60sec() {
+    var tottime = times.length;
+    var totsmall = 0;
+    for (var i = 0; i < tottime; i++) {
+        var time = times[i].split(":");
+        var hours = parseInt(time[0]);
+        var minutes = parseInt(time[1]);
+        var totmin = hours * 60 + minutes;
+        
+        var current = new Date();
+        var chkhours = current.getHours();
+        var chkminutes = current.getMinutes();
+        var chktotmin = chkhours * 60 + chkminutes;
+
+        if(totmin < chktotmin) {
+            totsmall++;
+        }
+    }
+    var round = totsmall/tottime*100
+    var out = 100 - Math.round(round);
+    $('#percent').each(function() {
+        $(this).text(out + "%");
+    });
+}
+fn60sec();
+setInterval(fn60sec, 60*1000);
