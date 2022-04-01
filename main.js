be456567e5263def83325c91eb861312b817419b
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2022-04-01 21:53:15 +0200

    weather

--- main.js
+++ main.js
@@ -22,6 +22,21 @@ function fn60sec() {
     $('#percent').each(function() {
         $(this).text(out + "%");
     });
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Waanrode&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, sys, weather } = data;
+            const sky = weather[0]['main'];
+            $('#weather').text(sky);
+            if(sky == 'Clouds') {
+                $(this).text("0%");
+            }
+        })
+        .catch((e) => {
+        console.log(e)
+        });
 }
 fn60sec();
 setInterval(fn60sec, 60*1000);
